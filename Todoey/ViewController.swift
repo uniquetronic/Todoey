a7d3ac5f24aa0b9ce615a2fadb8c38d8433f7bf1
Josip <uniquetronic@gmail.com>
2019-09-17 14:00:41 -0400

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Josip on 9/17/19.
+//  Copyright © 2019 Josip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
